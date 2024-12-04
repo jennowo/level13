fbfedb8d26cf18c98a5907eb41edbccc6d987a6a
Jennifer Wolfe <derpy.turtle.sandwich@gmail.com>
2024-12-03 21:18:35 -0800

    Fixed size, created menu loop, added 2 additional features

--- download.c
+++ download.c
@@ -3,11 +3,166 @@
 #include <libsocket/libinetsocket.h>
 #include <string.h>
 #include <unistd.h>
+#include <ctype.h>
+
+void ListFiles(FILE *s)  {
+    int bufferSize = 1000;
+    char buffer[bufferSize];
+
+    fprintf(s, "LIST\n");
+    fflush(s);
+
+    printf("\n%-30s%-30s", "Filename", "Size");             // Header
+    printf("\n---------------------------------------\n");
+
+    while (fgets(buffer, sizeof(buffer), s) != NULL) {   
+        if (strcmp(buffer, ".\n") == 0) {                   // Check for end-of-data marker
+            break;
+        }
+
+        int size;
+        char name[100];
+        int getSizeName = sscanf(buffer, "%d %s", &size, name);
+
+        // Make sure size and name was found before printing
+        if (getSizeName == 2) {
+            printf("\n%-30s", name); 
+            printf("%-30d", size); 
+            // TODO Print size in KB, GB, etc.
+        }
+    }
+}
+
+// Additional feature: Progress bar displays '-' for how far the download has progressed. Ex: [-------  ]
+void ProgressBar(int fileSize, int transferred) {
+    const int width = 20;                              // Bar width
+    // Find how many bars to display
+    float progress = (float)transferred / fileSize;  
+    int pos = width * progress;  
+
+    printf("\n["); 
+    for (int i = 0; i < width; ++i) {
+        if (i < pos) printf("-");
+        else printf(" ");
+    }
+    printf("] %3d%%\n\n", (int)(progress * 100));         // Percentage
+}
+
+void DownloadFile(FILE* s, int fd) {
+    int bufferSize = 1000;
+    char buffer[bufferSize];
+
+    // Get filename from user
+    char fileName[100];
+    printf("Which file do you want to download? ");
+    scanf("%s", fileName);
+
+    // Additional Feature: If the file already exists in the user's directory, ask if they want to overwrite it
+    if (access(fileName, F_OK) == 0) {
+        char choice;
+        printf("The file '%s' already exists. Input 'o' to overwrite: ", fileName);
+        scanf(" %c", &choice);
+        if (tolower(choice) != 'o') {
+            printf("'%s' will not be downloaded.\n", fileName);
+            return;
+        }
+    }
+
+    // Get SIZE of file
+    fprintf(s, "SIZE %s\n", fileName);
+    fflush(s);
+
+    if (fgets(buffer, sizeof(buffer), s) == NULL) {
+        printf("Failed to retrieve file size.\n");
+        fclose(s);
+        close(fd);
+        exit(1);
+    }
+
+    int fileSize = 0;
+    if (sscanf(buffer, "+OK %d", &fileSize) != 1 || fileSize <= 0) {
+        printf("Invalid file size.\n");
+        fclose(s);
+        close(fd);
+        exit(1);
+    }
+    printf("File size: %d bytes\n", fileSize);
+
+    // Send GET command for file
+    fprintf(s, "GET %s\n", fileName);
+    fflush(s);
+
+    // Check for errors in server response
+    if (fgets(buffer, sizeof(buffer), s) == NULL) {             // Check if fgets was success
+        printf("Data was not received.\n");
+        fclose(s);
+        close(fd);
+        exit(1);
+    }
+    if (strncmp(buffer, "-ERR", 4) == 0) {                      // Check for "-ERR"
+        printf("Error retrieving file: %s", buffer);
+        fclose(s);
+        close(fd);
+        exit(1);
+    }
+
+    // Save contents of server file to local file
+    FILE *destination = fopen(fileName, "wb");
+    if (!destination) {
+        printf("Could not open a file for writing.\n");
+        fclose(s);
+        close(fd);
+        exit(1);
+    }
+
+    int transferred = 0;                                        // Total amount transferred so far
+    while (transferred < bufferSize) {
+        int remaining = fileSize - transferred;
+        int bytesWanted;
+
+        // min(remaining, bufferSize)
+        if (remaining < bufferSize) {
+            bytesWanted = remaining;
+        } else {
+            bytesWanted = bufferSize;
+        }
+
+        // Read bytes wanted from server file into the buffer, check for errors?
+        int bytesReceived = fread(buffer, 1, bytesWanted, s);  
+        if (bytesReceived <= 0) {
+            printf("Error receiving data or unexpected end.\n");
+            break;        
+        }
+
+        // Write bytes received into dest file from the buffer
+        fwrite(buffer, 1, bytesReceived, destination);
+        transferred = transferred + bytesReceived;             // Update the amount transferred 
+
+        // Additional Feature: Show progress bar
+        ProgressBar(fileSize, transferred);
+
+    }
+    
+    printf("The file %s has been downloaded.\n", fileName);  
+}
+
+void Quit(FILE *s, int fd) {
+    fprintf(s, "QUIT\n");
+    fflush(s);
+    fclose(s);
+    close(fd);
+}
+
+void PrintMenu() {
+        printf("\n\nMenu:\n\t(L) List Files\n\t(D) Download\n\t(Q) Quit\n");
+        printf("\nWhat would you like to do? ");
+}
 
 int main() {
     // Ask which server, append server name and domain, open connection on port 3456
     char hostname[100] = "";
     int bufferSize = 1000;
+    char buffer[bufferSize];
 
     printf("Would you like to connect to London or Newark?\n");
     scanf("%s", hostname);
@@ -28,120 +183,26 @@ int main() {
         return 1;
     }
 
-    
-    // Send LIST for file listing, then print formatted file list
-    char buffer[bufferSize];
+    // Loop to show menu and prompt for choice until Q is selected
+    char choice;
+    do{
+        PrintMenu();
+        scanf(" %c", &choice);
 
-    fprintf(s, "LIST\n");
-    fflush(s);
-
-    printf("\n%-30s%-30s", "Filename", "Size");            // Header
-    printf("\n---------------------------------------\n");
-
-    while (fgets(buffer, sizeof(buffer), s) != NULL) {   
-        if (strcmp(buffer, ".\n") == 0) {                   // Check for end-of-data marker
-            break;
+        switch (tolower(choice)) {
+            case 'l':
+                ListFiles(s);
+                break;
+            case 'd':
+                DownloadFile(s, fd);
+                break;
+            case 'q':
+                Quit(s, fd);
+                break;
+            default:
+                printf("Invalid choice.\n");
         }
-
-        int size;
-        char name[100];
-        int getSizeName = sscanf(buffer, "%d %s", &size, name);
-
-        // Make sure size and name was found before printing
-        if (getSizeName == 2) {
-            printf("\n%-30s", name); 
-            printf("%-30d", size); 
-            // TODO Print size in KB, GB, etc.
-        }
-    }
-
-
-
-    // Get filename from user
-    char fileName[100];
-    printf("\nWhich file do you want to get? ");
-    scanf("%s", fileName);
-
-    // Send GET command with the filename
-    fprintf(s, "GET %s\n", fileName);
-    fflush(s);
-
-    // Check for errors in server response
-    int fileSize = 60; // Size of file
-
-    if (fgets(buffer, sizeof(buffer), s) == NULL) {             // Check if fgets was success
-        printf("Data was not received.\n");
-        fclose(s);
-        close(fd);
-        return 1;
-    }
-    printf("Server Response: %s", buffer);                      // TEST raw server response
-    if (strncmp(buffer, "-ERR", 4) == 0) {                      // Chck for "-ERR"
-        printf("Error retrieving file: %s", buffer);
-        fclose(s);
-        close(fd);
-        return 1;
-    }
-    /* if (sscanf(buffer, "+OK %d", &fileSize) != 1) {             // Get file size
-        printf("Server Response: %s", buffer);                  // TEST raw server response
-        printf("Invalid response from server: %s", buffer);
-        fclose(s);
-        close(fd);
-        return 1;
-    } */
-
-    /* // Get file size
-    fprintf(s, "SIZE %s\n", fileName);
-    fflush(s);
-    char fileSize[100];
-    char sizeBuffer[100];
-    fgets(sizeBuffer, 100, s);
-    printf("%s", sizeBuffer);
-    sscanf(sizeBuffer, "+OK %s", fileSize); 
-
-    printf("File size %s", fileSize); */
-
-    // Save contents of server file to local file
-    FILE *destination = fopen(fileName, "wb");
-    if (!destination) {
-        printf("Could not open a file for writing.\n");
-        fclose(s);
-        close(fd);
-        return 1;
-    }
-
-    int transferred = 0;                                        // Total amount transferred so far
-
-    while (transferred < bufferSize) {
-        int remaining = fileSize - transferred;
-        int bytesWanted;
-
-        // min(remaining, bufferSize)
-        if (remaining < bufferSize) {
-            bytesWanted = remaining;
-        } else {
-            bytesWanted = bufferSize;
-        }
-
-        // Read bytes_wanted from server file into the buffer
-        int bytesReceived = fread(buffer, 1, bytesWanted, s);  
-        if (bytesReceived <= 0) {
-            printf("Error receiving data or unexpected end.\n");
-            break;          // TODO idk about this one
-        }
-
-        // write bytes_received into dest file from the buffer
-        fwrite(buffer, 1, bytesReceived, destination);
-        transferred = transferred + bytesReceived;             // Update the amount transferred 
-
-    }
-    
-    printf("\nThe file %s has been downloaded.\n", fileName); 
-    
-    // Close connection
-    fprintf(s, "QUIT\n");
-    fflush(s);
-    fclose(s);
+    } while(tolower(choice) != 'q');
 
     return 0;
 }
